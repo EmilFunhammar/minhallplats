8b3d8a3ba74a400adbb923cc08c68946920a96d1
EmilFunhammar <emil.funhammar@gmail.com>
2019-11-13 18:05:32 +0100

    kometarer
    lagt till komentarer

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ hamtaelemnet();
 //få site id från sl
 function getsiteid() {
   const span = document.getElementById("information");
-  sparaelement(span.value);
+  sparaelement(span.value); // ger värdet till funtionen sparaelement
   const div = document.getElementById("siteid");
   const url1 =
     "https://cors-anywhere.herokuapp.com/https://api.sl.se/api2/typeahead.json?key=bed5fbd50fab49febec03d10dd35a8f5&searchstring=" +
@@ -17,11 +17,9 @@ function getsiteid() {
       let infos = data.ResponseData;
 
       return infos.map(function(info) {
-        div.innerHTML = info.SiteId; //+ "<br>" + info.X + "<br>" + info.Y;
-        getrealinfo();
-        hidebuttons();
-
-        //info.Name; //+ " " + info.SiteId + " " + info.X + " " + info.Y;
+        div.innerHTML = info.SiteId; //hämtar siteid från trafiklabs api
+        getrealinfo(); // kör funktion
+        hidebuttons(); // kör funktion
       });
     })
     .catch(function(error) {
@@ -34,15 +32,12 @@ function getsiteid() {
 //realtid info
 function getrealinfo() {
   const span = document.getElementById("siteid").innerHTML;
-
   const div = document.getElementById("real");
-
   div.innerHTML = "<tr><th>linje</th><th>Gå om</th><th>Nästa avgång</th></tr>";
   const url =
     "https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=73415dbcf95348e79eb8399b7f51d6c4&siteid=" +
     span +
     "&timewindow=60";
-  console.log(span);
   fetch(url) //hämtar realtid
     .then(resp => resp.json()) //berättar att filen är json dokumnet
     .then(function(data) {
@@ -50,35 +45,40 @@ function getrealinfo() {
         infos = data.ResponseData.Trams;
       } else if (document.getElementById("buss").checked == true) {
         infos = data.ResponseData.Buses;
-      }
-
-      //let infos = data.ResponseData.Trams;
-      // if stats fr bytevar text = "";
+      } // kollar checkboxes ifall tåg eller buss ska lässas in
       return infos.map(function(info) {
         if (mytime(info.ExpectedDateTime) > 0) {
+          // if (trafiktime(info.DisplayTime) > 0) {
           div.innerHTML +=
             "<tr><td>" +
             info.LineNumber +
             " " +
             info.Destination +
             "</td><td>" +
-            mytime(info.ExpectedDateTime) +
+            mytime(info.ExpectedDateTime) + // ger värdet rill funktionen mytime
             " min" +
             "</td><td>" +
             info.DisplayTime +
             "</td></tr>";
-        }
-
-        //bättre förklairngx
-        //getdistens();
+        } // skappar en tabel i html men informationen från json filen
       });
     })
     .catch(function(error) {
       console.log(error);
     });
 }
-//
+/* function trafiktime(DisplayTime) {
+  var date1 = new Date();
+  var date2 = Date(DisplayTime);
+  console.log(date2);
+  
 
+  var minutediff = Math.round((date2 - date1) / 1000 / 60);
+  return minutediff;
+} */
+//
+//
+// omvanldar så det står minter och beräknar gå avståndet
 function mytime(ExpectedDateTime) {
   var date1 = new Date();
   var date2 = Date.parse(ExpectedDateTime);
@@ -90,10 +90,9 @@ function mytime(ExpectedDateTime) {
   if (diff == 1 + " min") {
     diff = "nu";
   }
-  //console.log(minutediff);
-  //console.log(date2);
   return diff;
 }
+
 //
 //
 //Hämtar data
@@ -107,7 +106,7 @@ function hamtaelemnet() {
       let info = data.data;
 
       return info.map(function(info) {
-        document.getElementById("information").value = info.data1;
+        document.getElementById("information").value = info.data1; // ger värdet i json filen samma värde som skrivs in i min informations ruta
       });
     })
     .catch(function(error) {
@@ -134,6 +133,7 @@ function sparaelement(stationsplats) {
       console.log(error);
     });
 }
+// gömmer och visar block
 function hidebuttons() {
   document.getElementById("hide").style.display = "none";
   document.getElementById("backbutton").style.display = "block";
